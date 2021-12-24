aa42d08a2e79a53584bf2b13e03f4ebd7f8f28d0
SalyginVS <salyginvs@gmail.com>
2021-12-24 13:07:44 +0200

    unit_02

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
-/* console.log('Hello!'); // take strings in brackets
-console.log('World!');
-console.log('3333');
-console.log(334);
-console.log("Hellow" + " " + "world"); // cocatenation operation
-console.info("info");
+let inputIn = document.querySelector('.input-in');
+let button = document.querySelector('button');
+let out = document.querySelector('div.out');
 
-// alert('Hello');
-
-document.getElementById("out").innerHTML = '<i>italic</i>';
-document.querySelector('h2.header').innerHTML = "headr";
-
-let a = document.querySelector('#one');
-a.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing \
-    elit. Impedit autem explicabo voluptate inventore quam aliquam porro ea, corporis facere placeat \
-    laboriosam tempore rerum deleniti tempora sapiente voluptates velit ut assumenda."; */
-
-document.querySelector('h1').innerHTML = "Lesson-2 Home Work";
-
-console.log('Vladimir');
-console.log(8);
-console.log('Добро ' + 'пожаловать ' + ' на курс');
-
-// alert(2019);
-
-// alert(2019 - 300);
+button.onclick = function() {
+  let input = +inputIn.value;
+  console.log(input);
+  out.innerHTML = input;
+  inputIn.value = ''
+}
